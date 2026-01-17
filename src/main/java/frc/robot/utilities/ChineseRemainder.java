7e001d7e4fe1c891f5d6fafa6d1ec6ef72eda670
Jason Smith <jasonpatricksmith00@gmail.com>
2026-01-17 12:57:06 -0500

    first try at calculation (untested)

--- src/main/java/frc/robot/utilities/ChineseRemainder.java
+++ src/main/java/frc/robot/utilities/ChineseRemainder.java
@@ -9,6 +9,32 @@ public class ChineseRemainder {
         double RotatedTeeth1 = RotationsEnc1.getRotations() * TotalTeeth1;
         double RotatedTeeth2 = RotationsEnc2.getRotations() * TotalTeeth2;
 
-        return 0.0;
+        double bestResultDifference = 1000.0; // closer to 0 = more accurate. Both calculations are the most similar
+        double currentResult = 0;
+
+        /*
+         * N = i*k1 + n1
+         * N = j*k2 + n2
+         */
+
+        for (int i = 0; i < TotalTeeth2; i++) { // finding i
+            double test = i * TotalTeeth1 + RotatedTeeth1;
+            double remainderCheck = test % TotalTeeth2;
+
+            if (Math.abs(remainderCheck - RotatedTeeth2) < bestResultDifference) {
+                currentResult = test;
+            }
+        }
+
+        for (int i = 0; i < TotalTeeth1; i++) { // finding i
+            double test = i * TotalTeeth2 + RotatedTeeth2;
+            double remainderCheck = test % TotalTeeth1;
+
+            if (Math.abs(remainderCheck - RotatedTeeth1) < bestResultDifference) {
+                currentResult = test;
+            }
+        }
+
+        return currentResult;
     }
 }
