d37d7b4cd9451ee078c7aab4b84432b4ab1e16e4
oliver <oliverlu201003@gmail.com>
2026-01-16 19:41:02 -0800

    use realIntakeWheels, idk what im doing AHHHH, what is a motor

---
+++ src/main/java/frc/robot/subsystems/realIntakeWheels.java
@@ -0,0 +1,21 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+/**integrate motors and tell them to run, when button is pressed, then brake!! */
+
+
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class realIntakeWheels extends SubsystemBase {
+  /** Creates a new realIntakeWheels. */
+  public realIntakeWheels() {}
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
