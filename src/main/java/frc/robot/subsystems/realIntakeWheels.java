b7f3bdd17cf721b1fabf133ab7e30ae50a43e9c6
oliver <oliverlu201003@gmail.com>
2026-01-16 20:36:26 -0800

    for Holmes

--- src/main/java/frc/robot/subsystems/realIntakeWheels.java
+++ src/main/java/frc/robot/subsystems/realIntakeWheels.java
@@ -4,16 +4,38 @@
 
 /**integrate motors and tell them to run, when button is pressed, then brake!! */
 
-
-
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.signals.InvertedValue;
+
+import frc.robot.Constants;
 
 public class realIntakeWheels extends SubsystemBase {
+  private final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();  
+  private final TalonFX m_motor;
+  private static final double K_P = 3.0;
+  
   /** Creates a new realIntakeWheels. */
-  public realIntakeWheels() {}
+  public realIntakeWheels() {
+    m_motor = new TalonFX(Constants.INTAKE_CAN_ID);
+    Slot0Configs slot0configs = talonFXConfigs.Slot0;
+    slot0configs.kP = K_P;  // start small!!!
+    slot0configs.kI = 0.0;
+    slot0configs.kD = 0.0;
 
+    // enable brake mode (after main config)
+    m_motor.getConfigurator().apply(talonFXConfigs);
+    m_motor.setNeutralMode(NeutralModeValue.Brake);
+
+    MotorOutputConfigs m_motor = new MotorOutputConfigs();
+    m_motor.Inverted = InvertedValue.Clockwise_Positive;
+  }
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
