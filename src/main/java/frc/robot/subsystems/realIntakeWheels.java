0510d7b12f3c61d49472bd52da6ef53c0899aa25
Hsholmes <HolmesHuang15@outlook.com>
2026-01-16 20:39:13 -0500

    Apply Current Limit

--- src/main/java/frc/robot/subsystems/realIntakeWheels.java
+++ src/main/java/frc/robot/subsystems/realIntakeWheels.java
@@ -11,6 +11,7 @@ import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.signals.InvertedValue;
 
@@ -20,6 +21,7 @@ public class realIntakeWheels extends SubsystemBase {
   private final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();  
   private final TalonFX m_motor;
   private static final double K_P = 3.0;
+  private static final int CURRENT_LIMIT = 90;
   
   /** Creates a new realIntakeWheels. */
   public realIntakeWheels() {
@@ -28,6 +30,11 @@ public class realIntakeWheels extends SubsystemBase {
     slot0configs.kP = K_P;  // start small!!!
     slot0configs.kI = 0.0;
     slot0configs.kD = 0.0;
+
+    CurrentLimitsConfigs currentLimits = new CurrentLimitsConfigs()
+            .withSupplyCurrentLimit(CURRENT_LIMIT)
+            .withStatorCurrentLimit(CURRENT_LIMIT);
+        talonFXConfigs.withCurrentLimits(currentLimits);
 
     // enable brake mode (after main config)
     m_motor.getConfigurator().apply(talonFXConfigs);
