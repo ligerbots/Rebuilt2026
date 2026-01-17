5bcb692a7e12c88e57b8d45cb8da7e01bf9d12f7
Jason Smith <jasonpatricksmith00@gmail.com>
2026-01-16 20:37:41 -0500

    added constants, motionmagic, currentlimits, set

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -6,16 +6,49 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.MotionMagicVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
 
 public class Hood extends SubsystemBase {
+
+  private Rotation2d m_goal = Rotation2d.fromDegrees(0.0);
+
+  private final TalonFX m_hoodMotor;
+
+  private static final double SUPPLY_CURRENT_LIMIT = 40;
+  private static final double STATOR_CURRENT_LIMIT = 60;
+
+  private static final double K_P = 1.0;
+
+
   /** Creates a new Hood. */
-  public Hood() {}
+  public Hood() {
+    m_hoodMotor = new TalonFX(0); // FIXME change 0 to Constants.java value
 
-  private Rotation2d goalAngle = Rotation2d.fromDegrees(0.0);
+    // set config to factory default
+    m_hoodMotor.getConfigurator().apply(new TalonFXConfiguration());
+        
+    TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
 
-  // CTRE Talon FX configuration for the Kraken X44 (Talon FX compatible)
-  private final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
+    talonFXConfigs.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT_LIMIT;
+    talonFXConfigs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT;
+    
+    Slot0Configs slot0configs = talonFXConfigs.Slot0;
+    slot0configs.kP = K_P;
+    slot0configs.kI = 0.0;
+    slot0configs.kD = 0.0;
+
+    m_hoodMotor.getConfigurator().apply(talonFXConfigs);
+    m_hoodMotor.setPosition(0);
+  }
 
   @Override
   public void periodic() {
@@ -23,13 +56,11 @@ public class Hood extends SubsystemBase {
   }
 
   public void set(Rotation2d angle) {
-    goalAngle = angle;
+    m_goal = angle;
+    m_hoodMotor.setControl(new MotionMagicVoltage(m_goal.getRotations()));
   }
 
-  public Rotation2d readAngle() {
-
-    return goalAngle; // example
+  public Rotation2d getPosition(){
+    return Rotation2d.fromRotations(m_hoodMotor.getPosition().getValueAsDouble());
   }
-
-
 }
