7fa81fc7ddf3f49b8e45777d42092c442bcd72fa
Jason Smith <jasonpatricksmith00@gmail.com>
2026-01-16 20:08:24 -0500

    pheonix v6 + talonfx import

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -6,12 +6,16 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
 
 public class Hood extends SubsystemBase {
   /** Creates a new Hood. */
   public Hood() {}
 
-  Rotation2d goalAngle;
+  private Rotation2d goalAngle = Rotation2d.fromDegrees(0.0);
+
+  // CTRE Talon FX configuration for the Kraken X44 (Talon FX compatible)
+  private final TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
 
   @Override
   public void periodic() {
@@ -19,13 +23,13 @@ public class Hood extends SubsystemBase {
   }
 
   public void set(Rotation2d angle) {
-    
+    goalAngle = angle;
   }
 
   public Rotation2d readAngle() {
 
-    return new Rotation2d(2.1,2.3); // example
+    return goalAngle; // example
   }
 
-  
+
 }
