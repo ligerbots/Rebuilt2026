8f3449c31595313c531b68b128676d7170274c07
AlyHacks <alyasu929@gmail.com>
2026-01-18 16:56:38 -0500

    resolved comments

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -1,95 +1,84 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+//look for motor ratios
 package frc.robot.subsystems;
 
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-
-import static edu.wpi.first.units.Units.Rotation;
-
-import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.MotionMagicVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
-import com.ctre.phoenix6.signals.NeutralModeValue;
-//review
-import com.ctre.phoenix6.configs.MotionMagicConfigs;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
-
 
 
 public class Turret extends SubsystemBase {
 
   private Rotation2d m_goal = Rotation2d.fromDegrees(0.0);
 
-  private final TalonFX m_hoodMotor;
+  private final TalonFX m_turretMotor;
 
-  private static final double SUPPLY_CURRENT_LIMIT = 40;
-  private static final double STATOR_CURRENT_LIMIT = 60;
+  private static final double SUPPLY_CURRENT_LIMIT = 60;
+  private static final double STATOR_CURRENT_LIMIT = 40;
 
-  private static final double K_P = 1.0;
-  private static final double MAX_VEL_METER_PER_SEC = Units.inchesToMeters(100.0);
-  private static final double MAX_ACC_METER_PER_SEC_SQ = Units.inchesToMeters(750.0);
-  private static final double MAX_JERK_METER_PER_SEC3 = Units.inchesToMeters(12500.0);
-  private Rotation2d m_maxRotation2d = Rotation2d.fromRadians(360.0);
-  private Rotation2d m_minRotation2d = Rotation2d.fromRadians(0.0);
+  private static final double K_P = 1.0; //TODO tune
+  private static final double MAX_VEL_ROT_PER_SEC = 1;//TODO add new rotations/sec instead of meters
+  private static final double MAX_ACC_ROT_PER_SEC_SQ = 0.5;
+  private static final Rotation2d MAX_ROTATION = Rotation2d.fromRadians(2 * Math.PI);
+  private static final Rotation2d MIN_ROTATION = Rotation2d.fromRadians(0.0);
 
 
   /** Creates a new Turret. */
   public Turret() {
-    m_hoodMotor = new TalonFX(0); // FIXME change 0 to Constants.java value
-
-    // set config to factory default
-    m_hoodMotor.getConfigurator().apply(new TalonFXConfiguration());
-        
+    m_turretMotor = new TalonFX(0); //TODO add the motor id constant
+  
     TalonFXConfiguration talonFXConfigs = new TalonFXConfiguration();
 
     talonFXConfigs.CurrentLimits.SupplyCurrentLimit = SUPPLY_CURRENT_LIMIT;
     talonFXConfigs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT;
     
     Slot0Configs slot0configs = talonFXConfigs.Slot0;
-    slot0configs.kP = K_P;
+    slot0configs.kP = K_P; //TODO tune
     slot0configs.kI = 0.0;
     slot0configs.kD = 0.0;
 
-    m_hoodMotor.getConfigurator().apply(talonFXConfigs);
-    m_hoodMotor.setPosition(0);
-
     MotionMagicConfigs magicConfigs = talonFXConfigs.MotionMagic;
         
-    magicConfigs.MotionMagicCruiseVelocity = MAX_VEL_METER_PER_SEC; // Target cruise velocity of 80 rps //TODO Change values TOTAL GUESSES 
-    magicConfigs.MotionMagicAcceleration = MAX_ACC_METER_PER_SEC_SQ; // Target acceleration of 160 rps/s (0.5 seconds)
-    magicConfigs.MotionMagicJerk = MAX_JERK_METER_PER_SEC3; // Target jerk of 1600 rps/s/s (0.1 seconds)
-  
+    magicConfigs.MotionMagicCruiseVelocity = MAX_VEL_ROT_PER_SEC; // Target cruise velocity of 80 rps //TODO Change values TOTAL GUESSES 
+    magicConfigs.MotionMagicAcceleration = MAX_ACC_ROT_PER_SEC_SQ; // Target acceleration of 160 rps/s (0.5 seconds) 
+    m_turretMotor.getConfigurator().apply(talonFXConfigs);
+    m_turretMotor.setPosition(0);
+
+    
   }
 
   @Override
-  public void periodic() {                                                 
+  public void periodic() {    
+    SmartDashboard.putNumber("turret/goalAngle", m_goal.getRadians());
+    SmartDashboard.putNumber("turret/currentAngle", getPosition().getRadians());
+    SmartDashboard.putNumber("turret/rawMotorAngle", m_turretMotor.getPosition().getValueAsDouble());
+    
+  
     // This method will be called once per scheduler run
   }
 
   public void set(Rotation2d angle) {
-    m_goal = angle;
-    m_hoodMotor.setControl(new MotionMagicVoltage(m_goal.getRotations()));
+    m_goal = limitRotation(angle);
+    m_turretMotor.setControl(new MotionMagicVoltage(m_goal.getRotations()));
   }
 
   public Rotation2d getPosition(){
-    return Rotation2d.fromRotations(m_hoodMotor.getPosition().getValueAsDouble());
+    return Rotation2d.fromRotations(m_turretMotor.getPosition().getValueAsDouble());
   }
 
    // set Motion Magic settings
     
 //max rotation and min rotation in constants
 //mathutil.clamp
-public Rotation2d limiterRotation(Rotation2d angle){
-  return Rotation2d.fromRadians(MathUtil.clamp(angle.getRadians(), m_minRotation2d.getRadians(), m_maxRotation2d.getRadians()));
+  private Rotation2d limitRotation(Rotation2d angle){
+    return Rotation2d.fromRadians(MathUtil.clamp(angle.getRadians(), MIN_ROTATION.getRadians(), MAX_ROTATION.getRadians()));
 }
 }
