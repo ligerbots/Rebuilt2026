7ec29eb3d3fa4c245ba16134f0dd1d3e4679ea90
Hsholmes <HolmesHuang15@outlook.com>
2026-01-19 11:55:20 -0500

    Create shooter feeder code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -3,6 +3,7 @@ package frc.robot;
 public class Constants {
     public static final int FLYWHEEL_CAN_ID = 9;
     public static final int INTAKE_ROLLER_CAN_ID = 17;
+    public static final int SHOOTER_FEEDER_CAN_ID = 18;
 
     // NOTE: TalonFX motors (all Krakens and Falcons) share the same ID space, so CANNOT repeat
     // CAN ID 1-8 for Talons are for the Swerve motors
