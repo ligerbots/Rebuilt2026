c744255b75aac98f1e29bda3d74a327b365206ba
Paul Rensing <prensing@ligerbots.org>
2026-01-19 09:41:41 -0500

    clean up comments

--- src/main/java/frc/robot/subsystems/Turret.java
+++ src/main/java/frc/robot/subsystems/Turret.java
@@ -24,9 +24,10 @@ public class Turret extends SubsystemBase {
   private static final double SUPPLY_CURRENT_LIMIT = 60;
   private static final double STATOR_CURRENT_LIMIT = 40;
 
-  private static final double K_P = 1.0; //TODO tune
-  private static final double MAX_VEL_ROT_PER_SEC = 1;//TODO add new rotations/sec instead of meters
-  private static final double MAX_ACC_ROT_PER_SEC_SQ = 0.5;
+  // TODO Tune the constants
+  private static final double K_P = 1.0; 
+  private static final double MAX_VEL_ROT_PER_SEC = 1;  // Target cruise velocity of 80 rps
+  private static final double MAX_ACC_ROT_PER_SEC_SQ = 0.5;   // Target acceleration of 160 rps/s (0.5 seconds) 
   private static final Rotation2d MAX_ROTATION = Rotation2d.fromRadians(2 * Math.PI);
   private static final Rotation2d MIN_ROTATION = Rotation2d.fromRadians(0.0);
 
@@ -41,28 +42,24 @@ public class Turret extends SubsystemBase {
     talonFXConfigs.CurrentLimits.StatorCurrentLimit = STATOR_CURRENT_LIMIT;
     
     Slot0Configs slot0configs = talonFXConfigs.Slot0;
-    slot0configs.kP = K_P; //TODO tune
+    slot0configs.kP = K_P;
     slot0configs.kI = 0.0;
     slot0configs.kD = 0.0;
 
     MotionMagicConfigs magicConfigs = talonFXConfigs.MotionMagic;
-        
-    magicConfigs.MotionMagicCruiseVelocity = MAX_VEL_ROT_PER_SEC; // Target cruise velocity of 80 rps //TODO Change values TOTAL GUESSES 
-    magicConfigs.MotionMagicAcceleration = MAX_ACC_ROT_PER_SEC_SQ; // Target acceleration of 160 rps/s (0.5 seconds) 
+    magicConfigs.MotionMagicCruiseVelocity = MAX_VEL_ROT_PER_SEC; 
+    magicConfigs.MotionMagicAcceleration = MAX_ACC_ROT_PER_SEC_SQ; 
+
     m_turretMotor.getConfigurator().apply(talonFXConfigs);
     m_turretMotor.setPosition(0);
-
-    
   }
 
+  // This method will be called once per scheduler run
   @Override
   public void periodic() {    
     SmartDashboard.putNumber("turret/goalAngle", m_goal.getRadians());
     SmartDashboard.putNumber("turret/currentAngle", getPosition().getRadians());
     SmartDashboard.putNumber("turret/rawMotorAngle", m_turretMotor.getPosition().getValueAsDouble());
-    
-  
-    // This method will be called once per scheduler run
   }
 
   public void set(Rotation2d angle) {
@@ -74,11 +71,9 @@ public class Turret extends SubsystemBase {
     return Rotation2d.fromRotations(m_turretMotor.getPosition().getValueAsDouble());
   }
 
-   // set Motion Magic settings
-    
-//max rotation and min rotation in constants
-//mathutil.clamp
-  private Rotation2d limitRotation(Rotation2d angle){
+  // max rotation and min rotation in constants
+  // mathutil.clamp
+  private Rotation2d limitRotation(Rotation2d angle) {
     return Rotation2d.fromRadians(MathUtil.clamp(angle.getRadians(), MIN_ROTATION.getRadians(), MAX_ROTATION.getRadians()));
-}
+  }
 }
