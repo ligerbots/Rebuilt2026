b16b793c02f56151e99a93b1f9c85d343fda9e6f
oliver <oliverlu201003@gmail.com>
2026-01-18 16:04:22 -0800

    fixed can id

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,6 @@
 package frc.robot;
 
 public class Constants {
-    public static final int INTAKE_CAN_ID = 5;  // Falcon500
+    public static final int FLYWHEEL_CAN_ID = 9;
+    public static final int INTAKE_CAN_ID = 17;  // Falcon500
 }
