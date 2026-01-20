907213e4e12baca97d115729842f17331eff1170
Hsholmes <HolmesHuang15@outlook.com>
2026-01-20 18:51:09 -0500

    log current distance

--- src/main/java/frc/robot/subsystems/ClimberArms.java
+++ src/main/java/frc/robot/subsystems/ClimberArms.java
@@ -110,12 +110,14 @@ public class ClimberArms extends SubsystemBase {
         SmartDashboard.putNumber("ClimberArms/goalDistanceLeft", m_goalDistanceLeft);
         SmartDashboard.putNumber("ClimberArms/goalRotationLeft", m_goalRotationLeft);
         SmartDashboard.putNumber("ClimberArms/currentRotationLeft", m_currentRotationLeft);
+        SmartDashboard.putNumber("ClimberArms/currentDistanceLeft", m_currentRotationLeft);
         
         SmartDashboard.putBoolean("ClimberArms/toTargetRight", onTarget(MotorSelection.RIGHT));
         SmartDashboard.putNumber("ClimberArms/currentRPMRight", getRPM(m_rightMotor));
         SmartDashboard.putNumber("ClimberArms/goalDistanceRight", m_goalDistanceRight);
         SmartDashboard.putNumber("ClimberArms/goalRotationRight", m_goalRotationRight);
         SmartDashboard.putNumber("ClimberArms/currentRotationRight", m_currentRotationRight);
+        SmartDashboard.putNumber("ClimberArms/currentDistanceRight", m_currentRotationRight);
 
     }
 
