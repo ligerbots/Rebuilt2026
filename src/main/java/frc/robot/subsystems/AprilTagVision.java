902f4ce4964b002b42db4c727cb9d7e62d46f614
Jason Smith <jasonpatricksmith00@gmail.com>
2026-01-20 20:08:11 -0500

    apriltagvision creation and passdown

--- src/main/java/frc/robot/subsystems/AprilTagVision.java
+++ src/main/java/frc/robot/subsystems/AprilTagVision.java
@@ -55,7 +55,7 @@ public class AprilTagVision {
     // static final AprilTagFields APRILTAG_FIELD = AprilTagFields.k2025ReefscapeWelded;
     static final AprilTagFields APRILTAG_FIELD = AprilTagFields.k2026RebuiltWelded;
 
-    static private final String CUSTOM_FIELD = "2025-reefscape-andymark_custom.json";
+    static private final String CUSTOM_FIELD = "2025-reefscape-andymark_custom.json"; // old
 
     // Use the multitag pose estimator
     static final PoseStrategy POSE_STRATEGY = PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR;
