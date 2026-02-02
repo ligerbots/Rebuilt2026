0ffbc3fd5654400ec945a900629e83c45f368812
jacobh1 <jake.hendrickson@gmail.com>
2026-02-01 21:37:42 -0500

    Add new auto command with multi-path sequence

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,19 +8,23 @@ import static edu.wpi.first.units.Units.MetersPerSecond;
 import static edu.wpi.first.units.Units.RadiansPerSecond;
 import static edu.wpi.first.units.Units.RotationsPerSecond;
 
+import java.util.Objects;
+
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 import frc.robot.commands.AutoCommandInterface;
-import frc.robot.commands.CtreTestAuto;
+import frc.robot.commands.FirstBasicAuto;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.AprilTagVision;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
@@ -49,78 +53,95 @@ public class RobotContainer {
     private final CommandSwerveDrivetrain m_drivetrain;
     private final AprilTagVision m_aprilTagVision = new AprilTagVision();
 
-    public RobotContainer() {
-        if (Robot.isSimulation()) {
-            DriverStation.silenceJoystickConnectionWarning(true);
+    private final SendableChooser<String> m_chosenFieldSide = new SendableChooser<>();
+    private int m_autoSelectionCode; 
+    
+        public RobotContainer() {
+            if (Robot.isSimulation()) {
+                DriverStation.silenceJoystickConnectionWarning(true);
+            }
+            
+            m_drivetrain = new CommandSwerveDrivetrain(
+                m_aprilTagVision,
+                TunerConstants.DrivetrainConstants,
+                TunerConstants.FrontLeft, TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight
+            );
+    
+            configureBindings();
+            configureAutos();
         }
-        
-        m_drivetrain = new CommandSwerveDrivetrain(
-            m_aprilTagVision,
-            TunerConstants.DrivetrainConstants,
-            TunerConstants.FrontLeft, TunerConstants.FrontRight, TunerConstants.BackLeft, TunerConstants.BackRight
-        );
-
-        configureBindings();
-    }
-
-    private void configureBindings() {
-        m_drivetrain.setupPathPlanner();
-
-        m_drivetrain.setDefaultCommand(getDriveCommand());
-
-        // Idle while the robot is disabled. This ensures the configured
-        // neutral mode is applied to the drive motors while disabled.
-        final var idle = new SwerveRequest.Idle();
-        RobotModeTriggers.disabled().whileTrue(
-            m_drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
-
-        // lock wheels
-        m_driverController.a().whileTrue(m_drivetrain.applyRequest(() -> m_brakeRequest));
-        // m_driverController.b().whileTrue(drivetrain.applyRequest(() ->
-        //     point.withModuleDirection(new Rotation2d(-m_driverController.getLeftY(), -m_driverController.getLeftX()))
-        // ));
-
-        // Run SysId routines when holding back/start and X/Y.
-        // Note that each routine should be run exactly once in a single log.
-        m_driverController.back().and(m_driverController.y()).whileTrue(m_drivetrain.sysIdDynamic(Direction.kForward));
-        m_driverController.back().and(m_driverController.x()).whileTrue(m_drivetrain.sysIdDynamic(Direction.kReverse));
-        m_driverController.start().and(m_driverController.y()).whileTrue(m_drivetrain.sysIdQuasistatic(Direction.kForward));
-        m_driverController.start().and(m_driverController.x()).whileTrue(m_drivetrain.sysIdQuasistatic(Direction.kReverse));
-
-        // Reset the field-centric heading on left bumper press.
-        m_driverController.leftBumper().onTrue(m_drivetrain.runOnce(m_drivetrain::seedFieldCentric));
-
-        m_drivetrain.registerTelemetry(m_logger::telemeterize);
-    }
-
-    // public Command getAutonomousCommand() {
-    //     // Simple drive forward auton
-    //     final var idle = new SwerveRequest.Idle();
-    //     return Commands.sequence(
-    //         // Reset our field centric heading to match the robot
-    //         // facing away from our alliance station wall (0 deg).
-    //         drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
-    //         // Then slowly drive forward (away from us) for 5 seconds.
-    //         drivetrain.applyRequest(() ->
-    //             drive.withVelocityX(0.5)
-    //                 .withVelocityY(0)
-    //                 .withRotationalRate(0)
-    //         )
-    //         .withTimeout(5.0),
-    //         // Finally idle for the rest of auton
-    //         drivetrain.applyRequest(() -> idle)
-    //     );
-    // }
-
-    public CommandSwerveDrivetrain getDriveTrain() {
-        return m_drivetrain;
-    }
-
-    public Command getAutonomousCommand() {
-        if(null==m_autoCommand) {
-            m_autoCommand = new CtreTestAuto(m_drivetrain, true);
+    
+        private void configureAutos() {
+            m_chosenFieldSide.setDefaultOption("Depot Side", "Depot Side");
+            m_chosenFieldSide.addOption("Outpost Side", "Outpost Side");
+    
+            SmartDashboard.putData("Field Side", m_chosenFieldSide);
         }
+    
+        private void configureBindings() {
+            m_drivetrain.setupPathPlanner();
+    
+            m_drivetrain.setDefaultCommand(getDriveCommand());
+    
+            // Idle while the robot is disabled. This ensures the configured
+            // neutral mode is applied to the drive motors while disabled.
+            final var idle = new SwerveRequest.Idle();
+            RobotModeTriggers.disabled().whileTrue(
+                m_drivetrain.applyRequest(() -> idle).ignoringDisable(true)
+            );
+    
+            // lock wheels
+            m_driverController.a().whileTrue(m_drivetrain.applyRequest(() -> m_brakeRequest));
+            // m_driverController.b().whileTrue(drivetrain.applyRequest(() ->
+            //     point.withModuleDirection(new Rotation2d(-m_driverController.getLeftY(), -m_driverController.getLeftX()))
+            // ));
+    
+            // Run SysId routines when holding back/start and X/Y.
+            // Note that each routine should be run exactly once in a single log.
+            m_driverController.back().and(m_driverController.y()).whileTrue(m_drivetrain.sysIdDynamic(Direction.kForward));
+            m_driverController.back().and(m_driverController.x()).whileTrue(m_drivetrain.sysIdDynamic(Direction.kReverse));
+            m_driverController.start().and(m_driverController.y()).whileTrue(m_drivetrain.sysIdQuasistatic(Direction.kForward));
+            m_driverController.start().and(m_driverController.x()).whileTrue(m_drivetrain.sysIdQuasistatic(Direction.kReverse));
+    
+            // Reset the field-centric heading on left bumper press.
+            m_driverController.leftBumper().onTrue(m_drivetrain.runOnce(m_drivetrain::seedFieldCentric));
+    
+            m_drivetrain.registerTelemetry(m_logger::telemeterize);
+        }
+    
+        // public Command getAutonomousCommand() {
+        //     // Simple drive forward auton
+        //     final var idle = new SwerveRequest.Idle();
+        //     return Commands.sequence(
+        //         // Reset our field centric heading to match the robot
+        //         // facing away from our alliance station wall (0 deg).
+        //         drivetrain.runOnce(() -> drivetrain.seedFieldCentric(Rotation2d.kZero)),
+        //         // Then slowly drive forward (away from us) for 5 seconds.
+        //         drivetrain.applyRequest(() ->
+        //             drive.withVelocityX(0.5)
+        //                 .withVelocityY(0)
+        //                 .withRotationalRate(0)
+        //         )
+        //         .withTimeout(5.0),
+        //         // Finally idle for the rest of auton
+        //         drivetrain.applyRequest(() -> idle)
+        //     );
+        // }
+    
+        public CommandSwerveDrivetrain getDriveTrain() {
+            return m_drivetrain;
+        }
+    
+        public Command getAutonomousCommand() {
+
+            int currentAutoSelectionCode = Objects.hash(m_chosenFieldSide.getSelected(),
+             DriverStation.getAlliance());
+     
+            // Only call constructor if the auto selection inputs have changed
+            if (m_autoSelectionCode != currentAutoSelectionCode) {
+                m_autoCommand = new FirstBasicAuto(m_drivetrain,
+                        m_chosenFieldSide.getSelected().equals("Depot Side"));
+            }
         return m_autoCommand;
     }
 
