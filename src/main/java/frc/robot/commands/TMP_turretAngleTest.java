d975b46093eab682e43a6e3c5f7c71df9c11d02a
Charles Frankston <cbf@mit.edu>
2026-02-13 21:32:15 -0500

    Revert "Revert "Get TMP_turretAngleTest working in simulator""
    This reverts commit 32bce3b736582620895ad84f3eff8ed6713ce154.

--- src/main/java/frc/robot/commands/TMP_turretAngleTest.java
+++ src/main/java/frc/robot/commands/TMP_turretAngleTest.java
@@ -34,14 +34,17 @@ public class TMP_turretAngleTest extends Command {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    Rotation2d rotationGoal = Turret.getTranslationToGoal(m_robotPose.get(), FieldConstants.HUB_POSITION_BLUE).getAngle();
+    Rotation2d rotationGoal = Turret.getTranslationToGoal(m_robotPose.get(),  FieldConstants.flipTranslation(FieldConstants.HUB_POSITION_BLUE)).getAngle();
+    SmartDashboard.putBoolean("TurretAngleTest", true);
     SmartDashboard.putNumber("turretTesting/ComputedAngle", rotationGoal.getDegrees());
     m_Turret.setAngle(rotationGoal);
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    SmartDashboard.putBoolean("TurretAngleTest", false);
+  }
 
   // Returns true when the command should end.
   @Override
