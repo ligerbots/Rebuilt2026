2c792d133030eda677685e1e1809b8a14eb6dae3
LigerBots <ligerbots@gmail.com>
2026-03-23 20:43:25 -0400

    shot tuning

--- src/main/java/frc/robot/subsystems/intake/IntakeRoller.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeRoller.java
@@ -24,7 +24,7 @@ public class IntakeRoller extends SubsystemBase {
     private static final Current SUPPLY_CURRENT_LIMIT = Amps.of(40);
     private static final Current STATOR_CURRENT_LIMIT = Amps.of(120);
     
-    private static final double INTAKE_VOLTAGE = 7.5;  // was 9
+    private static final double INTAKE_VOLTAGE = 6.0;  // was 9
     private static final double OUTTAKE_VOLTAGE = 6.0;
 
     private final TalonFX m_motor;
