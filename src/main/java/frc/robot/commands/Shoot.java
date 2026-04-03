b03b9936b393652b09e42cae27e74f7bcd508823
Paul Rensing <prensing@ligerbots.org>
2026-04-03 11:02:55 -0400

    Scale the LUT by 0.96 and reset the scale in the code

--- src/main/java/frc/robot/commands/Shoot.java
+++ src/main/java/frc/robot/commands/Shoot.java
@@ -45,7 +45,7 @@ public class Shoot extends Command {
     // seem to need to scale the TOF numbers down
     private static final double TOF_SCALE = 0.75;
 
-    private static final double FLYWHEEL_SCALE = 0.96; //was 0.98 Q7
+    private static final double FLYWHEEL_SCALE = 1.0;
 
     // for fixed shot only
     private final Translation2d m_fixedShotVector;
