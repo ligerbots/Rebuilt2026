153658e56f5eceb937bd69d6e89555524542ce59
AlyHacks <137717874+AlyHacks@users.noreply.github.com>
2026-06-06 08:29:36 -0400

    added state file

---
+++ src/main/java/frc/robot/State.java
@@ -0,0 +1,12 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+
+public class State {
+    
+
+
+}
